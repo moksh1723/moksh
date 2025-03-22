947e791f23e8d01ab0c4c091d2a6e53d4e474f75
moksh1723 <mokshpatil1704@gmail.com>
2025-03-22 14:01:41 +0530

    Add files via upload
    test commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let input = document.getElemenntById('inputbox');
+let buttons = document.querySelectorALL
+('button');
+let string ="";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+button.addEventListener('click', (e) =>){
+    if (e.target.inner)
+}
+})
